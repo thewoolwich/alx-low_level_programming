8001101767140428ad51f1059d76e918ffd9825d
thewoolwich <thewoolwich@gmail.com>
2023-06-19 09:41:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-char *message = "Programming is like building a multilingual puzzle"
-puts(message);
-
-return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
