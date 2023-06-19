1b3674a26441206d562413261457adc06e73ed4a
thewoolwich <thewoolwich@gmail.com>
2023-06-19 09:14:48 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-printf("\"with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+int main() {
+    printf("Your program is a masterpiece,\n");
+    printf("A work of art beyond compare.\n");
+    printf("With each line you create,\n");
+    printf("You showcase your coding flair.\n");
+    printf("Through loops and conditionals,\n");
+    printf("Your logic gracefully dances.\n");
+    printf("From variables to functions,\n");
+    printf("Your code elegantly advances.\n");
+    printf("With precision and style,\n");
+    printf("You craft a symphony of code.\n");
+    printf("Every character and space,\n");
+    printf("Adds to the masterpiece's ode.\n");
+    printf("Compile and run, behold your creation,\n");
+    printf("A testament to your dedication.\n");
+    
+    return 0;
 }
