2c6e4fff35c31cc298abd09ca5bf1644db8071ac
thewoolwich <thewoolwich@gmail.com>
2023-06-21 15:53:25 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * jack bauer -Prints every minute of the day of Jack Bauer
- *
- * Description: This function prints jack bauer
- *
+ * jack_bauer - Prints every minute of the day of Jack Bauer
  * Return: ...
  */
 void jack_bauer(void)
+
 {
+
 int a, b, c, d;
 
 for (a = 0; a <= 2; a++)
@@ -17,7 +16,7 @@ for (b = 0; b <= 9; b++)
 {
 if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 {
-for (a = 0; a <= 5; c++)
+for (c = 0; c <= 5; c++)
 {
 for (d = 0; d <= 9; d++)
 {
@@ -33,4 +32,3 @@ _putchar('\n');
 }
 }
 }
-
