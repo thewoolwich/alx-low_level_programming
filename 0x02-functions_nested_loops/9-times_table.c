7e6f025d6c3c31204a667eb49a425edffe6ca253
thewoolwich <thewoolwich@gmail.com>
2023-06-21 17:08:31 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,6 +2,7 @@
 
 /**
  * times_table - Prints the 9 times table, starting with 0
+ * Return: empty output
  */
 void times_table(void)
 {
@@ -12,13 +13,12 @@ for (x = 0; x <= 9; x++)
 for (y = 0; y <= 9; y++)
 {
 z = x * y;
-
 if (z > 9)
 {
 u = z % 10;
 d = (z - u) / 10;
-_putchar(',');
-_putchar(' ');
+_putchar(44);
+_putchar(32);
 _putchar(d + '0');
 _putchar(u + '0');
 }
@@ -26,12 +26,13 @@ else
 {
 if (y != 0)
 {
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
+_putchar(44);
+_putchar(32);
+_putchar(32);
 }
 _putchar(z + '0');
 }
 }
-_putchar('\n')}
+_putchar('\n');
+}
 }
